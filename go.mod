0c9a0d8ea005ee819aa60a362ea09c4ebe8a2c7d
Brian <brianjiang@liontravel.com>
2023-09-10 15:03:25 +0800

    create main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/o83213/puppy
+
+go 1.21.1
---
+++ puppy.go
@@ -0,0 +1,12 @@
+package puppy
+
+import "fmt"
+
+func Bark() {
+	fmt.Println("Bark!")
+}
+
+
+func Barks() {
+	fmt.Println("Bark! Bark! Bark!")
+}
